ccece3de17ad69377e779f48a91927c63a11d3a5
DHayes23 <D.Hayes23@hotmail.com>
2021-12-08 22:01:03 +0000

    Add manage_deck.html and placeholder form

--- app.py
+++ app.py
@@ -100,6 +100,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/manage_deck")
+def manage_deck():
+
+    return render_template("manage_deck.html")
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
