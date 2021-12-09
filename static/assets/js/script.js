226b2dd913ddfe6667af09eae6f5fd6c02d9d3ff
DHayes23 <D.Hayes23@hotmail.com>
2021-12-09 15:42:07 +0000

    Remove unnecessary repeated function call

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,7 +1,4 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-
-$(document).ready(function(){
-    $('select').formSelect();
-  });
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+  $('select').formSelect();
+});
